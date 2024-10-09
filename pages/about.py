ba0debf5b1007b745503cd7fcdf91a714b015219
ayadimounir <ayadimounir2008@gmail.com>
2024-10-09 10:26:45 +0200

    completed about and home

--- pages/about.py
+++ pages/about.py
@@ -3,7 +3,38 @@ from dash import html
 
 dash.register_page(__name__)
 
-layout = html.Div([
-    html.H1('This is our about page'),
-    html.Div('This is our about page content.'),
-])
+layout = html.Div(
+    [
+        html.H1(
+            'About us', 
+            style={
+                'textAlign': 'center', 
+                'fontSize': '36px',  # Increased font size for the title
+                'fontWeight': 'bold'
+            }
+        ),
+        
+        html.P(
+            [
+                "This website is based on a scientific work under publication, authored by ",
+
+                html.B("Mounir AYADI."),  # Highlight the name for emphasis
+                html.Br(),
+                "Mounir AYADI is a PhD candidate working at the University of Poitiers under the ",
+                html.Span("IC2MP EBicom team.", style={'fontStyle': 'italic'}),  # Italicize team name
+                html.Br(), html.Br(),
+                "Email: ",
+                html.A("mounir.ayadi@univ-poitiers.fr", href="mailto:mounir.ayadi@univ-poitiers.fr", style={'color': '#1E90FF'}),  # Make the email clickable and styled
+            ],
+            style={
+                'fontSize': '18px',
+                'lineHeight': '1.6',  # Increase line spacing for readability
+                'marginTop': '15px'
+            }
+        ),
+    ],
+    style={
+        'margin': '20px',
+        'fontFamily': 'Arial, sans-serif'
+    }
+)
--- pages/home.py
+++ pages/home.py
@@ -1,9 +1,52 @@
 import dash
-from dash import html
+from dash import html, dcc
 
 dash.register_page(__name__, path='/')
 
+# Layout definition
 layout = html.Div([
-    html.H1('This is our Home page'),
-    html.Div('This is our Home page content.'),
+    # Header Section
+    html.Div([
+        html.H1('Welcome to DBP Predictor: A prediction tool for byproducts formation during oxidation powered by Machine Learning', 
+                style={'text-align': 'center', 'margin-top': '20px'}),
+    ], style={'padding': '20px'}),
+
+    # Welcome Text Section
+    html.Div([        
+        html.P(
+            '''
+            DBP predictor is a website tool to help predict byproducts during wastewater chlorination.
+            Our predictions are made using various Machine Learning algorithms and real world collected data. 
+            Our best models perform with R2=0.968 when compared to real world measurements.
+            Click on "visualisation" to start exploring our predictions.
+            ''',
+            style={'font-size': '16px', 'line-height': '1.6', 'text-align': 'justify', 'padding': '0px 50px'}
+        ),
+    ], style={'padding': '30px'}),
+
+    # How to Use Section
+    html.Div([
+        html.H3('How to Use', style={'text-align': 'center'}),
+        html.P(
+            '''
+            Select a set of features used for prediction and a machine learning model from the drop down menus.
+            Select a target variable you wish to predict, and two axes on which you wish to see variations.
+            You can adjust the remaining sliders to input your desired water matrix. The graphic is fully interactive 
+            with Zoom, rotation, value highlighting, and real-time updates.
+            ''',
+            style={'font-size': '16px', 'line-height': '1.6', 'text-align': 'justify', 'padding': '0px 50px'}
+        ),
+    ], style={'padding': '30px'}),
+
+    # Construction Notice Section
+    html.Div([
+        html.H3('Note:', style={'text-align': 'center', 'color': '#d9534f'}),
+        html.P(
+            '''
+            This website is still under construction, please feel free to share your feedback via mail 
+            in the "About" section!
+            ''',
+            style={'font-size': '16px', 'line-height': '1.6', 'text-align': 'center', 'color': '#d9534f'}
+        ),
+    ], style={'padding': '20px'}),
 ])
